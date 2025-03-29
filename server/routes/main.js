bd565b6457c49ae06864fdb64a234c3ad9c43a65
rauxw <hardikraut909@gmail.com>
2025-03-29 18:02:09 +0530

    added posts and revertback

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,16 +1,49 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("../models/Post");
 
 // Routes
 
-router.get("", (req, res) => {
-  const locals = {
-    title: "NodeJs Blog",
-    description: "Simple Blog site",
-  };
+// Route for GET => HOME
 
-  res.render("index", { locals });
+router.get("", async function (req, res) {
+  try {
+    const locals = {
+      title: "NodeJs Blog",
+      description: "Simple Blog site",
+    };
+
+    let perPage = 6;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+    const count = await Post.countDocuments();
+    const nextPage = parseInt(page) + 1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+    res.render("index", {
+      locals,
+      data,
+      current: page,
+      nextPage: hasNextPage ? nextPage : null,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
+
+function insertPostData() {
+  Post.insertMany([
+    {
+      title: "Just a another test",
+      body: "This is just a body",
+    },
+  ]);
+}
 
 router.get("/about", (req, res) => {
   res.render("about");
