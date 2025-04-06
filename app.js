79298d12ffae7738762e73faa8456cd0214869b5
rauxw <hardikraut909@gmail.com>
2025-04-06 16:20:48 +0530

    admin route

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require("dotenv").config();
 
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 const connectDB = require("./server/config/db");
 
@@ -12,6 +15,19 @@ connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: "Keyboard cat",
+    resave: false,
+    saveUnitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URL,
+    }),
+  })
+);
+
 app.use(express.static("public"));
 
 // Templating Engine
@@ -20,6 +36,7 @@ app.set("layout", "./layout/main");
 app.set("view engine", "ejs");
 
 app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on: ${PORT}`);
---
+++ server/routes/admin.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const Post = require("../models/Post");
+const User = require("../models/User");
+
+const admin_layout = "../views/layout/admin";
+
+router.get("/admin", async (req, res) => {
+  try {
+    const locals = {
+      title: "Admin",
+      description: "Simple Blog page created with Node.js and MongoDB",
+    };
+    res.render("admin/index", { locals, layout: admin_layout });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/admin", async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
